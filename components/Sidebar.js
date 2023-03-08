26d5b4d8952945c5733584f7a2d2fae44274dad9
basioni <eng.ahmed.bassioni@gmail.com>
2023-03-08 22:25:44 +0200

    bootstrap installation

---
+++ components/Sidebar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Sidebar = () => {
+  return (
+    <nav id="sidebarMenu" class="d-md-block bg-light sidebar collapse align-left" >
+      <div class="position-sticky pt-3 sidebar-sticky">
+        <ul class="nav flex-column text-left">
+          <li class="nav-item">
+            <Link class="nav-link active" aria-current="page" href="/">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home align-text-bottom" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+              Dashboard
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" href="/orders">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file align-text-bottom" aria-hidden="true"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
+              Orders
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" href="/products">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart align-text-bottom" aria-hidden="true"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+              Products
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" href="/customers">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users align-text-bottom" aria-hidden="true"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+              Customers
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Sidebar
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,36 @@
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { useEffect } from "react";
+import Head from 'next/head';
+import Sidebar from '../components/Sidebar';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
+  return (
+    <>
+
+      
+      <div class="container-fluid text-center">
+        <div class="row">
+          <div class="col">
+          <Navbar></Navbar>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 col-lg-2">
+            <Sidebar/>
+          </div>
+          <div class="col-9">
+          <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
+      
+    </>
+  )
 }
+export default App;
